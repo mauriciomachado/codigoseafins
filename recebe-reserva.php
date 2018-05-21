ef889eef0d948300d0c40492f4c2e949fa73fd0f
mauriciomachado <mauricio@tutoweb.com.br>
2018-05-20 21:59:45 -0300

    Add files via upload

--- recebe-reserva.php
+++ recebe-reserva.php
@@ -6,9 +6,21 @@ header("Access-Control-Allow-Origin: *");
 
 
 //echo "<script>alert(\"CHEGOU!\"); window.close()</script>";
+
+
+$servidor = 'www.nicework.com.br';
+$usuario = 'nicew021_natan';
+$senha = '8LKTH3HO^~C=';
+$banco = 'nicew021_natan';
+
+// Conecta-se ao banco de dados MySQL
+$mysqli = new mysqli($servidor, $usuario, $senha, $banco);
+
+// Caso algo tenha dado errado, exibe uma mensagem de erro
+if (mysqli_connect_errno()) trigger_error(mysqli_connect_error());
 		
 
-include('conexao.php');
+//include('conexao.php');
 
 		$nome = $_POST["nome"];
 		$nome = addslashes($nome);
